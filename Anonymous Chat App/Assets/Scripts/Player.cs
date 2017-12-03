67441527787d84aeeaaab4497c320d462bf6ac7e
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-03 16:32:33 +0300

    Player Class added

---
+++ Anonymous Chat App/Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player{
+
+#region Private_Variables
+	private int _playerId;
+	private string _playerName;
+	private string _language;
+	private System.DateTime _createdDate;
+#endregion
+
+#region Public_Variables
+	public int playerId{
+		get{	return _playerId;	}
+	}
+
+	public string playerName{
+		get{	return _playerName;	}
+	}
+
+	public string language{
+		get{	return _language;	}
+	}
+#endregion
+
+#region Public_Methods
+
+#endregion
+}
