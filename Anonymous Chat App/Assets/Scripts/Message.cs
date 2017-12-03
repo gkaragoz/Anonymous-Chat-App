33f3fcbf64b6a6a4779db6e7a9b07a441b38d92b
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-03 16:40:43 +0300

    Message Class Added

---
+++ Anonymous Chat App/Assets/Scripts/Message.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Message{
+
+#region Private_Variables
+	private int _messageId;
+	private string _body;
+	private System.DateTime _sendDate;
+	private Talk _talk;
+#endregion
+
+#region Public_Variables
+	public int messageId{
+		get{	return _messageId;	}
+	}
+
+	public string body{
+		get{	return _body;		}
+	}
+
+	public System.DateTime sendDate{
+		get{	return _sendDate;	}
+	}
+
+	public Talk talk{
+		get{	return _talk;		}
+	}
+#endregion
+}
