1e4c6489a85a645e666947ad6c0b1ee123d6ef6f
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-07 17:24:52 +0300

    Initialize Talks Screen

--- Anonymous Chat App/Assets/Scripts/ChatPanelManager.cs
+++ Anonymous Chat App/Assets/Scripts/ChatPanelManager.cs
@@ -15,9 +15,31 @@ public class ChatPanelManager{
 		get{	return _user;	}
 	}
 
+	private Transform canvas;
+	private Transform talksParent;
+
 	public ChatPanelManager(Player user){
 		this._user = user;
 		instance = this;
+
+		this.canvas = GameObject.Find("Canvas").transform;
+		this.talksParent = this.canvas.Find("TalksScreen").Find("TalksListSection").GetChild(0).GetChild(0);
+
+		Debug.Log(this.canvas +  " : " + this.talksParent);
+		InitializeTalksScreen();
 	}
 	
+	public void InitializeTalksScreen(){
+		if(this.user == null)		return;
+
+		for(int ii = 0; ii < this.user.playerTalks.Length; ii++){
+			Talk currentTalk = this.user.playerTalks[ii];
+			Transform newConversation = (Transform)MonoBehaviour.Instantiate(Resources.Load<Transform>("Talks/Conversation"));
+			// newConversation.Find("talk_button").GetComponent<Button>().onClick();
+			newConversation.Find("user_name").GetComponent<Text>().text = currentTalk.recieverName;
+			newConversation.SetParent(this.talksParent);
+			newConversation.localPosition = Vector3.zero + new Vector3(532,-175 * ii,0);
+			newConversation.eulerAngles = Vector3.zero;
+		}
+	}
 }
