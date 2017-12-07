8a89a24663a4f3ade3d1d379a3a451ac738bf231
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-07 19:03:54 +0300

    AppStatus added

--- Anonymous Chat App/Assets/Scripts/AppManager.cs
+++ Anonymous Chat App/Assets/Scripts/AppManager.cs
@@ -10,10 +10,17 @@ public class AppManager : MonoBehaviour{
 	public static AppManager instance;
 
 	public Transform poolSystemParent;
+	
+	public AppStatus appStatus;
+	public enum AppStatus{
+		LOGIN,
+		TALK_SCREEN,
+		CONVERSATION
+	}
 
 	private void Awake(){
 		instance = this;
-		
+
 		TextAsset playerData = Resources.Load<TextAsset>("ExamplePlayer");
 		JSONObject playerJson = new JSONObject(playerData.text);
 
