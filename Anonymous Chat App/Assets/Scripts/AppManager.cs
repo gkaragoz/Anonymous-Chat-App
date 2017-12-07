3270afc7eb761d2d896a232df32c421db505a97e
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-07 17:24:38 +0300

    Initialize ChapPanel

--- Anonymous Chat App/Assets/Scripts/AppManager.cs
+++ Anonymous Chat App/Assets/Scripts/AppManager.cs
@@ -7,4 +7,13 @@ using UnityEngine;
 
 public class AppManager : MonoBehaviour{
 
+	private void Awake(){
+		TextAsset playerData = Resources.Load<TextAsset>("ExamplePlayer");
+		JSONObject playerJson = new JSONObject(playerData.text);
+
+		Player me = new Player(playerJson);
+		Debug.Log(me.playerName + " : " + me.playerId + " : " + me.playerTalks.Length);
+
+		new ChatPanelManager(me);
+	}
 }
