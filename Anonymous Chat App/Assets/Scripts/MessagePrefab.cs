27ca264ecf533af2e137d3dc6e7de47a945e0f9d
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-07 18:03:26 +0300

    Message Prefab Class

---
+++ Anonymous Chat App/Assets/Scripts/MessagePrefab.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessagePrefab : MonoBehaviour{
+
+	public bool myMessage;
+	public bool isUsing = false;
+	
+	public Text msjText;
+}
