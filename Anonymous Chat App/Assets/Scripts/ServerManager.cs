8a2aac31be616a188d3414db25403688f58cb806
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-15 04:17:08 +0300

    Server Manager Get User Data & Register Requests

---
+++ Anonymous Chat App/Assets/Scripts/ServerManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using ARW;
+using ARW.Com;
+using ARW.Config;
+using ARW.Users;
+using ARW.Events;
+using ARW.Requests.Extensions;
+
+public class ServerManager : MonoBehaviour{
+
+	public static ServerManager instance;
+
+	public ARWServer arwServer;
+
+	private void Awake(){
+		arwServer = new ARWServer();
+		arwServer.Init();
+
+		ConfigData cfg = new ConfigData(8081, 9933, "localhost");
+
+		arwServer.AddEventHandlers(ARWEvents.CONNECTION, OnConnectionSuccess);
+		arwServer.AddEventHandlers(ARWEvents.LOGIN, OnLoginSuccess);
+		arwServer.AddExtensionRequest("GetUserData", GetUserData);
+
+		instance = this;
+		arwServer.Connect(cfg);
+	}
+
+	private void Update(){
+		arwServer.ProcessEvents();
+	}
+
+	private void OnConnectionSuccess(ARWObject obj, object value){
+		Debug.Log("Connection Success");
+		AppManager.instance.appStatus = AppManager.AppStatus.Connection;
+
+		arwServer.SendLoginRequest("0", null);
+	}
+
+	private void OnLoginSuccess(ARWObject obj, object value){
+		Debug.Log("Login Success");
+		Debug.Log("Sending GetUserData Request");
+
+		obj = new IARWObject();
+		obj.PutString("player_id", "123123123");
+		obj.PutString("player_nickname", "powerLED");
+		obj.PutString("language", Application.systemLanguage.ToString());
+
+		arwServer.SendExtensionRequest("GetUserData", obj, false);
+	}
+
+	private void GetUserData(ARWObject obj, object value){
+		if(obj.GetString("error") == ""){
+			AppManager.instance.InitPlayer(obj.GetString("player_data"));
+		}else{
+			Debug.Log("GetUserData Error : " + obj.GetString("error"));
+		}
+	}
+}
