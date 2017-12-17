5cfebd3d6cc8cd6e8c22728b77fea70631736fed
gkaragoz <gkaragoz96@gmail.com>
2017-12-17 20:14:11 +0300

    Create PlayServicesManager script and add into Main scene

---
+++ Anonymous Chat App/Assets/Scripts/PlayServicesManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public class PlayServicesManager : MonoBehaviour {
+
+	void Start () {
+        SignIn();
+	}
+	
+	public void SignIn()
+    {
+        // authenticate user:
+        Social.localUser.Authenticate((bool success) => {
+            // handle success or failure
+            Debug.Log("Google Play LOGIN: Success");
+        });
+    }
+
+    public string GetId()
+    {
+        if (Social.localUser.authenticated)
+            return Social.localUser.id;
+        else
+            return "-1";
+    }
+}
